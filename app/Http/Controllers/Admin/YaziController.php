6c6a5ef5768f279b07da3942091dc91d6670d9cc
Anıl Şenocak Kubuntu <senocakanil@gmail.com>
2019-09-14 16:07:40 +0300

    YazıController'a admin kontolü için yapıcı metotta middleware eklendi

--- app/Http/Controllers/Admin/YaziController.php
+++ app/Http/Controllers/Admin/YaziController.php
@@ -1,9 +1,19 @@
 <?php
-namespace App\Http\Controllers\Admin;
-use Illuminate\Http\Request;
-use App\Http\Controllers\Controller;
+namespace App\Http\Controllers\Admin; 
+use App\Http\Controllers\Controller; 
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Redirect;
 
 class YaziController extends Controller{
+    public function __construct(){ 
+        $this->middleware(function ($request, $next) {  
+            if (Auth::user()->is_admin == 0) {
+                Redirect::to('/')->send();
+                abort(404);
+            }
+            return $next($request);
+        });   
+    }
     public function index(){
         return view("admin.index");
     }
